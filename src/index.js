5ddd83862b9420764bf18a66516ab3a23d89bd95
Eeveeee <fx8300gfe@gmail.com>
2020-08-06 19:14:36 +0300

    burger + adaptive look upgrade + header fixes

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import "./scss/index.scss";
 import "../node_modules/jquery/dist/jquery";
 import "../node_modules/popper.js/dist/popper";
+import "../src/js/burger";
 import "bootstrap";
---
+++ src/js/burger.js
@@ -0,0 +1,14 @@
+const burgerButton = document.querySelector(".burger-button");
+const burgerMenu = document.querySelector(".header-navbar");
+burgerButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  burgerButton.classList.toggle("button-active");
+  burgerMenu.classList.toggle("burger-active");
+  burgerMenu.classList.forEach((el) => {
+    if (el === "burger-active") {
+      document.body.style = "overflow:hidden";
+    } else {
+      document.body.style = "overflow:auto";
+    }
+  });
+});
